92dbef4780a12d8a887f0d97e5c407c0e76f9bed
Victor Häggqvist <victor@snilius.com>
2014-02-10 11:40:55 +0100

    .

---
+++ src/main/java/com/ecsoft/asteroids/controller/Render.java
@@ -0,0 +1,12 @@
+package com.ecsoft.asteroids.controller;
+
+/**
+ * Name: Asteroids
+ * Description: Render
+ *
+ * @author: Victor Häggqvist
+ * @since: 2/10/14
+ * Package: com.ecsoft.asteroids.controller
+ */
+public class Render {
+}
--- src/main/java/com/ecsoft/asteroids/model/Position.java
+++ src/main/java/com/ecsoft/asteroids/model/Position.java
@@ -1,13 +1,15 @@
 package com.ecsoft.asteroids.model;
 
+import java.awt.geom.Point2D;
+
 /**
  * Name: Asteroids
- * Description: Player
+ * Description: Position
  *
  * @author: Victor Häggqvist
  * @since: 2/10/14
  * Package: com.ecsoft.asteroids.model
  */
-public class Player {
+public class Position {
 
 }
