18b3dad84a64bd671e8a60eeed9501242b825c53
Victor Häggqvist <victor@snilius.com>
2014-02-10 14:20:04 +0100

    loop draft

---
+++ src/main/java/com/ecsoft/asteroids/controller/Loop.java
@@ -0,0 +1,24 @@
+package com.ecsoft.asteroids.controller;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Name: Asteroids
+ * Description: Loop
+ *
+ * @author: Victor Häggqvist
+ * @since: 2/10/14
+ * Package: com.ecsoft.asteroids.controller
+ */
+public class Loop {
+    private Timer timer;
+
+    public Loop() {
+
+    }
+
+    public void update(){
+
+    }
+}
