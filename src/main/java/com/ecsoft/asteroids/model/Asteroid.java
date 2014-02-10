4ad6187ceff15d21bb21a3df3c08bc3f282abd01
albinka <albinka@kth.se>
2014-02-10 14:05:58 +0100

    sadas

--- src/main/java/com/ecsoft/asteroids/model/Asteroid.java
+++ src/main/java/com/ecsoft/asteroids/model/Asteroid.java
@@ -1,5 +1,7 @@
 package com.ecsoft.asteroids.model;
 
+import java.awt.geom.Point2D;
+
 /**
  * Name: Asteroids
  * Description: Aseteroid
@@ -9,7 +11,45 @@ package com.ecsoft.asteroids.model;
  * Package: com.ecsoft.asteroids.controller
  */
 public class Asteroid {
-	public Asteroid() {
 
+    private Point2D position;
+    private Point2D velocity;
+    private int size;
+
+
+
+    /**
+     * Creates a new asteroid at a random location with a random velocity
+     * @param x Width of the game screen
+     * @param y Heght of the game screen
+     */
+	public Asteroid(int x, int y) {
+        this.size = 4;
+        this.position = new Point2D.Float((float)(Math.random()*x),(float)(Math.random()*y));
+        this.velocity = new Point2D.Float((float)(Math.random()),(float)(Math.random()));
 	}
+
+    /**
+     * Creates a new asteroid at the specified location and size
+     * @param x X-position of the asteroid
+     * @param y Y-position of the asteroid
+     * @param size Size of the asteroid
+     */
+    public Asteroid(int x, int y, int size) {
+        this.size = size;
+        this.position = new Point2D.Float(x,y);
+        this.velocity = new Point2D.Float((float)(Math.random()),(float)(Math.random()));
+    }
+
+    /**
+     * Updates the position of the asteroid
+     */
+    public void updatePos () {
+        position.setLocation(position.getX() + velocity.getX(), position.getY() + velocity.getY());
+    }
+
+
+
+
+
 }
