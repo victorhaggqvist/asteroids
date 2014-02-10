0f99d1ce335c199ac36e2a4ed4fd76a191f21cdb
Victor Häggqvist <victor@snilius.com>
2014-02-10 11:17:44 +0100

    wop

---
+++ src/main/java/com/ecsoft/asteroids/model/woop.java
@@ -0,0 +1,12 @@
+package com.ecsoft.asteroids.model;
+
+/**
+ * Name: Asteroids
+ * Description: woop
+ *
+ * @author: Victor Häggqvist
+ * @since: 2/10/14
+ * Package: com.ecsoft.asteroids.model
+ */
+public class woop {
+}
