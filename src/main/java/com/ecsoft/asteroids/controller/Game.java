4c1e79ba4cb188e309fdcbcf65f11a116983a4cc
Victor Häggqvist <victor@snilius.com>
2014-02-10 14:39:01 +0100

    game constoller

---
+++ src/main/java/com/ecsoft/asteroids/controller/Game.java
@@ -0,0 +1,15 @@
+package com.ecsoft.asteroids.controller;
+
+/**
+ * Name: Asteroids
+ * Description: Game
+ *
+ * @author: Victor Häggqvist
+ * @since: 2/10/14
+ * Package: com.ecsoft.asteroids.controller
+ */
+public class Game {
+    public Game() {
+
+    }
+}
