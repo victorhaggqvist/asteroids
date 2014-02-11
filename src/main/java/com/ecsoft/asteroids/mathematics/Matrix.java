05eeb72d175818961b56464c100b0d5e89262bf3
Peter Lundberg <peter.m.lundberg@gmail.com>
2014-02-11 15:12:46 +0100

    Matrix fixed for float and Point2D.Float[]

--- src/main/java/com/ecsoft/asteroids/mathematics/Matrix.java
+++ src/main/java/com/ecsoft/asteroids/mathematics/Matrix.java
@@ -1,9 +1,25 @@
 package com.ecsoft.asteroids.mathematics;
 
+import java.awt.geom.Point2D;
+
 public class Matrix {
     
     //Print matrix
     public static void print(double [][] x){
+        
+        for(int i = 0; i < x.length; i++){
+            for(int j = 0; j < x[0].length; j++){
+            
+                System.out.print(x[i][j] + " ");
+                
+            }
+            System.out.println();
+        }
+        
+    }
+    
+    
+    public static void print(float [][] x){
         
         for(int i = 0; i < x.length; i++){
             for(int j = 0; j < x[0].length; j++){
@@ -36,6 +52,61 @@ public class Matrix {
         return matrix;
         
     }
+    
+    public static float[][] convertPoint2DArrayTo2DMatrix(Point2D.Float[] x) {
+    	
+    	int rows = x.length;
+    	
+    	float[][] matrix = new float[rows][2];
+    	
+    	for(int i = 0; i < rows; i++) {
+    			matrix[i][0] = x[i].x;
+    			matrix[i][1] = x[i].y;
+    	}
+    	
+    	return matrix;
+    }
+    
+    
+    //M . x = b     Transformation
+    public static float[][] Transform(float[][] m, float[][] x){
+        
+        //Creating size for returning matrix
+        int rows = x.length;
+        int columns = x[0].length;
+        
+        float[][] matrix = new float[rows][columns];
+        
+        try{
+    
+            //Transforming transposed "x" matrix with transformation matrix "m"
+            //Also Transposing in the same process
+            for(int i = 0; i < m.length; i++){
+                for(int k = 0; k < x.length; k++){
+                    
+                    float t = 0;
+                    
+                    for(int j = 0; j < m[0].length; j++){
+                        
+                        //Flipt k and j to skip transposing
+                        t = t + m[i][j]*x[k][j];
+                    
+                    }
+                    
+                    //Flipt k and i to skip transposing
+                    matrix[k][i] = t;
+                }
+            }
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            //If matrix dimension does not match it will throw a ArrayIndexOutOfBoundsException
+            throw new ArrayIndexOutOfBoundsException("Matrix dimension does not match");
+            
+        }
+        return matrix;
+        
+    }
+    
     
     //M . x = b     Transformation
     public static double[][] Transform(double[][] m, double[][] x){
