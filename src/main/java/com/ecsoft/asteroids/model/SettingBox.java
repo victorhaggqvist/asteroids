edb5b211a599def5a26286efd89a491859476ff5
Victor Häggqvist <victor@snilius.com>
2014-02-11 15:32:10 +0100

    woop

---
+++ src/main/java/com/ecsoft/asteroids/model/SettingBox.java
@@ -0,0 +1,12 @@
+package com.ecsoft.asteroids.model;
+
+/**
+ * Name: Asteroids
+ * Description: SettingBox
+ *
+ * @author: Victor Häggqvist
+ * @since: 2/11/14
+ * Package: com.ecsoft.asteroids.model
+ */
+public class SettingBox {
+}
