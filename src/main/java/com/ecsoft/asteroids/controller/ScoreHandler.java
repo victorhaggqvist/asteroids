0f874e37e78272b194d4f4f855f400618606b34f
albinka <albinka@kth.se>
2014-02-25 13:16:21 +0100

    Revert "a working score :D"
    This reverts commit f936399e999a137fa94d7f4aa3ebc4b4b93b5a26.

--- src/main/java/com/ecsoft/asteroids/controller/ScoreHandler.java
+++ src/main/java/com/ecsoft/asteroids/controller/ScoreHandler.java
@@ -62,31 +62,22 @@ public final class ScoreHandler {
 	public static void addScore(HighScore highScore) throws IOException {
 		getHiscores();
 
-        if (highScore.getScore()>0){
-            if (highScoreList.getList().get(0).getScore()==0)
-                highScoreList.getList().remove(0);
 
-            //Update highScores
-            for (int i = 0; i < highScoreList.getList().size(); i++) {
-                // if new score is better than score at i insert it at i
-                if (highScore.getScore()>highScoreList.getList().get(i).getScore()){
-                    highScoreList.getList().add(i,highScore);
-                    break;
-                }
+
+        //Update highScores
+        for (int i = 0; i < highScoreList.getList().size(); i++) {
+            // if new score is better than score at i insert it at i
+            if (highScore.getScore()>highScoreList.getList().get(i).getScore()){
+                highScoreList.getList().add(i,highScore);
+                break;
             }
-
-            if (highScoreList.getList().size()<1)
-                highScoreList.getList().add(highScore);
-
-            highScoreList.getList().trimToSize();
-            if (highScoreList.getList().size()>10)
-                highScoreList.setList(new ArrayList<HighScore>(highScoreList.getList().subList(0, 10)));
-
-            //Writes to file
-            PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(FILE_NAME)));
-            Gson gson = new Gson();
-            pw.println(gson.toJson(highScoreList));
-            pw.close();
         }
+        highScoreList.getList().add(highScore);
+		
+		//Writes to file
+		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(FILE_NAME)));
+        Gson gson = new Gson();
+        pw.println(gson.toJson(highScoreList));
+		pw.close();
 	}
 }
