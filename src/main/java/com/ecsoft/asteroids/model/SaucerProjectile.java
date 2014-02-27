cfdd9ed6c742b2f09e1fff926d437caff4be976e
albinka <albinka@kth.se>
2014-02-27 12:57:38 +0100

    More difficulty settings stuff

--- src/main/java/com/ecsoft/asteroids/model/SaucerProjectile.java
+++ src/main/java/com/ecsoft/asteroids/model/SaucerProjectile.java
@@ -16,6 +16,7 @@ public class SaucerProjectile extends Projectile{
     private Point2D.Float position;
     private double direction;
     private long time;
+    private SettingsManager settings;
     
 
     /**
@@ -25,9 +26,11 @@ public class SaucerProjectile extends Projectile{
      * @param y
      * @param direction The direction specified in radians
      */
-    public SaucerProjectile(float x, float y, double direction) {
+    public SaucerProjectile(float x, float y, double direction) {        
     	super(x, y, direction);
-    	super.velocity = 15;
-    	super.timeToLive = 800;
+    	settings = SettingsManager.getInstance();
+    	super.velocity = 10+(settings.getDifficulty()*2);
+    	super.timeToLive = 1000+settings.getDifficulty()*400;
+    	
     }    
 }
